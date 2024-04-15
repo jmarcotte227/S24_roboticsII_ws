25f324dfe05e71798e65daca39e4367f2bd33251
jmarcotte227 <95249839+jmarcotte227@users.noreply.github.com>
2024-04-15 13:23:45 -0400

    Update maze_mapping.launch.py

--- src/snr/launch/maze_mapping.launch.py
+++ src/snr/launch/maze_mapping.launch.py
@@ -37,8 +37,8 @@ def generate_launch_description():
         '/map_gmapping_a1_launch.py']),
         
     )
-    package_path = get_package_share_path('snr')
-    default_rviz_config_path = package_path / 'rviz/map.rviz'
+    package_path = get_package_share_directory('snr')
+    default_rviz_config_path = os.path.join(package_path, 'map.rviz')
     rviz_arg = DeclareLaunchArgument(name='rvizconfig', default_value=str(default_rviz_config_path),
                                      description='Absolute path to rviz config file')
 
