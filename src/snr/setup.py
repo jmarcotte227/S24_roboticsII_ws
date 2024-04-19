d5306e18006cd84033824fde5b67458675dcba68
wizardninja21 <allisa@rpi.edu>
2024-04-19 17:51:32 -0400

    Update setup.py

--- src/snr/setup.py
+++ src/snr/setup.py
@@ -24,7 +24,7 @@ setup(
     license='TODO: License declaration',
     tests_require=['pytest'],
     entry_points={
-        'console_scripts': [ "nav_attempt = snr.nav_attempt.py:main"
+        'console_scripts': [ "nav_attempt = snr.nav_attempt:main"
         ],
     },
 )
