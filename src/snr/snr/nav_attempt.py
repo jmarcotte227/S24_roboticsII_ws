290e906b4e71eafcf69db39d6eeac9f7420b6be3
wizardninja21 <allisa@rpi.edu>
2024-04-19 19:40:07 -0400

    Update nav_attempt.py

--- src/snr/snr/nav_attempt.py
+++ src/snr/snr/nav_attempt.py
@@ -83,7 +83,7 @@ class TrackingNode(Node):
         # Create a subscriber to the detected object pose
         self.sub_detected_obj_pose = self.create_subscription(PoseStamped, '/detected_color_object_pose', self.detected_obj_pose_callback, 10)
 
-        self.sub_scan = self.create_subscription(LaserScan, '/scan',10)
+        self.sub_scan = self.create_subscription(LaserScan, '/scan', self.registerScan, 10)
     
         # Create timer, running at 100Hz
         self.timer = self.create_timer(0.01, self.timer_update)
@@ -142,7 +142,6 @@ class TrackingNode(Node):
         return object_pose
     ##from yahboom
     def registerScan(self, scan_data):
-        if not isinstance(scan_data, LaserScan): return
         self.ranges = np.array(scan_data.ranges)
    # self.Right_warning = 0
     #self.Left_warning = 0
